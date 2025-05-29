a67a93e2012314c34705b2f9bd17b490b97b6035
g-bartolome <132186400+g-bartolome@users.noreply.github.com>
2025-05-29 12:49:39 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+// Aquí iría la lógica para manejar los registros (simulada)
+function mostrarPrimero() {
+  alert("Mostrar primer registro (simulado)");
+}
+
+function mostrarAnterior() {
+  alert("Mostrar registro anterior (simulado)");
+}
+
+function mostrarSiguiente() {
+  alert("Mostrar siguiente registro (simulado)");
+}
+
+function mostrarUltimo() {
+  // Redirigir a portada.html
+  window.location.href = 'portada.html';
+}
